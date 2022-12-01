176a093e25e245435cc3917f96d5648bbbd58f61
Xaival <54257745+Xaival@users.noreply.github.com>
2022-12-01 05:51:42 +0100

    2022/12/01 04:51

--- Tiswagos Liri/Bloqueador de Spam con host.bat
+++ Tiswagos Liri/Bloqueador de Spam con host.bat
@@ -1111,6 +1111,7 @@ mode con:cols=65 lines=16
   echo 0.0.0.0 download-ready.net #AntiSpam>>%systemroot%\System32\drivers\etc\hosts 
   echo 0.0.0.0 drectsearch.com #AntiSpam>>%systemroot%\System32\drivers\etc\hosts 
   echo 0.0.0.0 dt69sx.xyz #AntiSpam>>%systemroot%\System32\drivers\etc\hosts 
+  echo 0.0.0.0 easy-ad-blocker.net #AntiSpam>>%systemroot%\System32\drivers\etc\hosts 
   echo 0.0.0.0 easyadblocker.info #AntiSpam>>%systemroot%\System32\drivers\etc\hosts 
   echo 0.0.0.0 ezadblocker.com #AntiSpam>>%systemroot%\System32\drivers\etc\hosts 
   echo 0.0.0.0 flowsearch.info #AntiSpam>>%systemroot%\System32\drivers\etc\hosts 
