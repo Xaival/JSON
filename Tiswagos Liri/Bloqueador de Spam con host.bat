9899c1bf13cc247afd65dd0fe0168cd8355970ed
Xaival <54257745+Xaival@users.noreply.github.com>
2023-02-07 19:30:19 +0100

    2023/02/07 18:30

--- Tiswagos Liri/Bloqueador de Spam con host.bat
+++ Tiswagos Liri/Bloqueador de Spam con host.bat
@@ -1136,6 +1136,7 @@ mode con:cols=65 lines=16
   echo 0.0.0.0 get.convertorsearch.com #AntiSpam>>%systemroot%\System32\drivers\etc\hosts 
   echo 0.0.0.0 getflowgreen.net #AntiSpam>>%systemroot%\System32\drivers\etc\hosts 
   echo 0.0.0.0 getsecuritysuite.com #AntiSpam>>%systemroot%\System32\drivers\etc\hosts 
+  echo 0.0.0.0 globaladblocker.com #AntiSpam>>%systemroot%\System32\drivers\etc\hosts 
   echo 0.0.0.0 greenadblocker.com #AntiSpam>>%systemroot%\System32\drivers\etc\hosts 
   echo 0.0.0.0 install.myincognitosearch.com #AntiSpam>>%systemroot%\System32\drivers\etc\hosts 
   echo 0.0.0.0 install.mystreamssearch.com #AntiSpam>>%systemroot%\System32\drivers\etc\hosts 
